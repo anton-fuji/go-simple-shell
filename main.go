09363500b17aca0bfa7518dc79350fe67b32c464
Fuji <fujiji.entire1414@gmail.com>
2025-11-30 21:10:37 +0900

    cmd ls

--- main.go
+++ main.go
@@ -20,13 +20,13 @@ func main() {
 		}
 
 		// ここでcat が入力されてコマンドが実行される処理を書く
-		if err := execInput(input); err != nil {
+		if err := cmdInput(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 }
 
-func execInput(input string) error {
+func cmdInput(input string) error {
 	// 改行を削除して前後の空白を無くしていく
 	input = strings.TrimSpace(input)
 
@@ -41,6 +41,8 @@ func execInput(input string) error {
 	switch command {
 	case "cat":
 		return cmdCat(args[1:])
+	case "ls":
+		return cmdLs(args[1:])
 	case "exit":
 		fmt.Println("Bye!!")
 		os.Exit(0)
@@ -70,6 +72,30 @@ func cmdCat(args []string) error {
 		file.Close()
 		if err != nil {
 			return fmt.Errorf("cat: %s %v", filename, err)
+		}
+	}
+
+	return nil
+}
+
+func cmdLs(args []string) error {
+	dir := "."
+	if len(args) > 0 {
+		dir = args[0]
+	}
+
+	// ディレクトリないのファイルを一覧取得j
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("ls : %v", err)
+	}
+
+	// ファイル名を表示していく
+	for _, entry := range entries {
+		if entry.IsDir() {
+			fmt.Printf("%s/\n", entry.Name())
+		} else {
+			fmt.Println(entry.Name())
 		}
 	}
 
